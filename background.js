ef9254d52975206e25fbe52b0029b2dc2bb08058
sayantan3 <sayantand975@gmail.com>
2022-08-01 07:48:17 +0530

    update

--- background.js
+++ background.js
@@ -1,21 +1,5 @@
-/*
-window.addEventListener('load', ()=> {
-  let sitesToBlock = [
-    'nytimes.com'
-  ];
-  let regexToMatchTLD = /\.[^.]+$/;
-  const domain = location.hostname.replace(regexToMatchTLD, '');
-  let href = window.location.href;
-  for (let i = 0; i < sitesToBlock.length; i++) {
-    if (href.includes(sitesToBlock[i])) {
-      let domain = sitesToBlock[i].replace(regexToMatchTLD, '');
-    }
-  }
-}, false);
 
-*/
-
-console.log("Loaded extension");
+console.log("connected");
 
 
 function blockRequest(details) {
