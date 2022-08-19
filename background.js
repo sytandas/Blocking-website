945f641c7bb08ec4364b5e0742da919e40e9c208
sayantan3 <sayantand975@gmail.com>
2022-08-19 22:56:32 +0530

    update

--- background.js
+++ background.js
@@ -1,19 +1,7 @@
 console.log("connected");
-/*
-chrome.declarativeNetRequest.updateDynamicRules({
-  addRules: [{
-    'id': 1,
-    'priority': 1,
-    'action': {'type': 'block'},
-    'condition': {
-      //'urlFilter': 'twitter',
-      'domains' : ["twitter.com"],
-      'resourceTypes': [
-        'csp_report', 'font', 'image', 'main_frame', 'media', 'object', 'other', 'ping', 'script',
-        'stylesheet', 'sub_frame', 'webbundle', 'websocket', 'webtransport', 'xmlhttprequest'
-      ]
-    }
-  }],
-  removeRuleIds: [1001]
-})
-*/
+
+window.addEventListener('load', ()=>{
+document.body.style.filter = 'grayscale(100%)';
+});
+//filter: greyscale(100%);
+
